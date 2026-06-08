e5435a5aca8b55bc0e903b01552c446a91a176aa
Temitope Thompson <topethompson2019@gmail.com>
2026-06-08 03:45:00 -0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t ttttttt534/demo-repo:jma-2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
+        sh 'docker push ttttttt534/demo-repo:jma-2.0'
     }
 }
 
